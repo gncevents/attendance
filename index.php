4e75ef4b10182a314dd26603ec2c6d5270f20c48
GNC Admin <gncadmin@amp.local>
2017-04-24 16:32:08 +0530

    printing server det index

--- index.php
+++ index.php
@@ -44,8 +44,12 @@ $tbl="";
 	}
 	$conn_str = getenv('SQLAZURECONNSTR_attendance');
 	$dbConn = connStrToArray($conn_str);
+	foreach($dbConn as $key => $value){
+        echo "<br />".$key.":".$value;
+	}
 
 	$serverName = substr($dbConn["Data Source"],4,34);
+	echo $serverName;
 	$connectionInfo = array( "Database"=>$dbConn["Database"], "UID"=>$dbConn["User Id"], "PWD"=>$dbConn["Password"]); 
  	$link = sqlsrv_connect( $serverName, $connectionInfo ) or die("Can not Login");
 
