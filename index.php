3d2c42f58d03cf0ca46da9365250b782c06a9e6d
GNC Admin <gncadmin@amp.local>
2017-04-25 16:12:58 +0530

    updated index and index2

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
+<?php date_default_timezone_set('Asia/Kolkata'); 
+$ipAddress=$_SERVER['REMOTE_ADDR'];
+$macAddr=false;
+
+$arp=`arp -a $ipAddress`;
+$lines=explode("\n", $arp);
+
+foreach($lines as $line)
+{
+   $cols=preg_split('/\s+/', trim($line));
+   if ($cols[0]==$ipAddress)
+   {
+       $macAddr=$cols[1];
+   }
+}
+echo $macAddr;
+?>
 <!DOCTYPE html>
 <html>
 <title>GNC Attendance System</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="./w3.css">
 <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
   <style>
         * {
 			font-family: Poppins !important;
@@ -31,21 +49,3 @@ $.ajax({
 </script>
 </body>
 </html>
-
-<?php date_default_timezone_set('Asia/Kolkata'); 
-$ipAddress=$_SERVER['REMOTE_ADDR'];
-$macAddr=false;
-
-$arp=`arp -a $ipAddress`;
-$lines=explode("\n", $arp);
-
-foreach($lines as $line)
-{
-   $cols=preg_split('/\s+/', trim($line));
-   if ($cols[0]==$ipAddress)
-   {
-       $macAddr=$cols[1];
-   }
-}
-echo $macAddr;
-?>
