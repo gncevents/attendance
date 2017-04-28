89f63ce5698499dd114370244c4f3a510dd3aef5
niravmadariya <niravpatel2facebook@gmail.com>
2017-04-28 11:57:25 +0530

    bug fix

--- Add.php
+++ Add.php
@@ -132,6 +132,6 @@ body{
 			</button>
 			</form>
 		</div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 </body>
 </html>
