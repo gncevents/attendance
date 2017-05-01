1167f7afdedd995824ba1c1f3f867a60c4893e99
niravmadariya <niravpatel2facebook@gmail.com>
2017-05-01 17:14:26 +0530

    testing new way to db connection

---
+++ cls_require.php
@@ -0,0 +1,34 @@
+<?php
+function connStrToArray($connStr){
+    $connArray = array();
+    $parts = explode(";", $connStr);
+    foreach($parts as $part){
+        $temp = explode("=", $part);
+        $connArray[$temp[0]] = $temp[1];
+    }
+    return $connArray;
+}
+$conn_str = getenv('SQLAZURECONNSTR_attendance');
+$dbConn = connStrToArray($conn_str);
+
+$serverName = substr($dbConn["Data Source"],4,34);
+
+$connectionInfo = array( "Database"=>$dbConn["Initial Catalog"], "UID"=>$dbConn["User ID"], "PWD"=>$dbConn["Password"]); 
+
+$link = sqlsrv_connect( $serverName, $connectionInfo ) or die("Can not Login");
+
+function query($quer){
+    $stmt = sqlsrv_query($quer);
+    if($stmt!=false){
+        return $stmt;
+    }
+    else{
+        return false;
+    }
+}
+function getdataassoc($stmt){
+    $row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC);
+    return $row;
+}
+
+?>
