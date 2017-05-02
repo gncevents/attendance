e7aeefc078aa3378f518fec4b1be239d5274c750
niravmadariya <niravpatel2facebook@gmail.com>
2017-05-02 16:21:11 +0530

    try credentials using single file

--- test.php
+++ test.php
@@ -1,14 +1,18 @@
 <?php
 include 'cls_require.php';
 $stmt = query("select * from attendance");
-while($row=getdataassoc($stmt)){
+if($stmt==false){
+	echo "false";
+}else{
+	while($row=getdataassoc($stmt)){
 		foreach($row as $key => $value){
 			echo $key.":".$value."<br />";
 		}
 		echo "<br />";
 	}
-	if($tbl==""){
-		echo $_GET['data'];
-		exit;
-	}
+}
+/*if($tbl==""){
+	echo $_GET['data'];
+	exit;
+}*/
 ?>
