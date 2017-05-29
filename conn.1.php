250e5aa59d38cb31af4f57418208afaeb363e402
NiravMadariya <niravpatel2facebook@gmail.com>
2017-05-30 01:01:29 +0530

    implemented new class with salary

--- conn.1.php
+++ conn.1.php
@@ -1,5 +1,5 @@
 <?php 
-function connStrToArray($connStr){
+function connStrToArray2($connStr){
     $connArray = array();
     $parts = explode(";", $connStr);
     foreach($parts as $part){
@@ -9,7 +9,7 @@ function connStrToArray($connStr){
     return $connArray;
 }
 $conn_str2 = getenv('SQLAZURECONNSTR_attendance1');
-$dbConn2 = connStrToArray($conn_str2);
+$dbConn2 = connStrToArray2($conn_str2);
 $serverName2 = substr($dbConn2["Data Source"],4,35);
 $connectionInfo2 = array( "Database"=>$dbConn2["Initial Catalog"], "UID"=>$dbConn2["User ID"], "PWD"=>$dbConn2["Password"]);
 $link2 = sqlsrv_connect( $serverName2, $connectionInfo2);
